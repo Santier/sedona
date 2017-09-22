a030c1bf9b82ca1cfc134245986f07963fa708e1
Alexander Krivenchik <zukhan@mail.ru>
2017-09-22 07:43:42 +0300

    Last commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
-var btn = document.querySelector('.btn-footer');
-if(btn){
-  var modal = document.querySelector('.write-us');
-  var close = document.querySelector('.close');
-  var overlay = document.querySelector('.overlay');
-  btn.addEventListener('click', function(e){
-    e.preventDefault();
-    modal.classList.toggle('hidden');
-    overlay.classList.toggle('hidden');
-  })
-  close.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  })
-  overlay.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  })
-  window.addEventListener('keydown', function(event){
-    if(event.keyCode === 27 && !modal.classList.contains('hidden')){
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
-    }
-  })
-}
+var btn = document.querySelector('.search-hotels-button');
+var modal = document.querySelector('.modal-window');
+
+btn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modal.classList.toggle('animation-show');
+  modal.classList.toggle('animation-hide');
+})
+
+// var btn = document.querySelector('.btn-footer');
+// if(btn){
+//   var modal = document.querySelector('.write-us');
+//   var close = document.querySelector('.close');
+//   var overlay = document.querySelector('.overlay');
+//   btn.addEventListener('click', function(e){
+//     e.preventDefault();
+//     modal.classList.toggle('hidden');
+//     overlay.classList.toggle('hidden');
+//   })
+//   close.addEventListener('click', function(){
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   })
+//   overlay.addEventListener('click', function(){
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   })
+//   window.addEventListener('keydown', function(event){
+//     if(event.keyCode === 27 && !modal.classList.contains('hidden')){
+//       modal.classList.add('hidden');
+//       overlay.classList.add('hidden');
+//     }
+//   })
+// }
